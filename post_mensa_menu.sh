4501eaf57590391ed6dfac03201cda83cb884969
Marvin Williams <marvin.williams@kit.edu>
2021-09-02 18:33:25 +0200

    fixup! initial commit

--- post_mensa_menu.sh
+++ post_mensa_menu.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 set -euo pipefail
 
-mensa_url='https://www.sw-ka.de/de/essen/?view=ok&STYLE=popup_plain&c=adenauerring&p=1&kw='$kw
-mri_url='https://casinocatering.de/speiseplan/'
-
 kw=$(date +%V)
 dow=$(date +%u)
+
+mensa_url='https://www.sw-ka.de/de/essen/?view=ok&STYLE=popup_plain&c=adenauerring&p=1&kw='$kw
+mri_url='https://casinocatering.de/speiseplan/'
 
 food_mensa=$(xsltproc --html --novalid mensa.xslt <(curl "${mensa_url}"))
 food_mri=$(xsltproc --html --novalid --param dow $dow mri.xslt <(curl "${mri_url}"))
