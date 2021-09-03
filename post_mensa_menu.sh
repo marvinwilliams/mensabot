9a1a00a816a886b43c225f3d44839bc386422c57
Marvin Williams <marvin.williams@kit.edu>
2021-09-03 09:45:51 +0200

    changed to update post instead of new post

--- post_mensa_menu.sh
+++ post_mensa_menu.sh
@@ -10,5 +10,5 @@ mri_url='https://casinocatering.de/speiseplan/'
 food_mensa=$(xsltproc --html --novalid mensa.xslt <(curl "${mensa_url}"))
 food_mri=$(xsltproc --html --novalid --param dow $dow mri.xslt <(curl "${mri_url}"))
 
-message='{"channel_id":"'"${MM_CHANNEL_ID}"'", "message":"'Mensa\\n\\n"${food_mensa}"\\n\\nMri\\n\\n"${food_mri}"'"}'
-echo $message | curl -H "Authorization: Bearer ${BOT_TOKEN}" -H 'Content-Type: application/json' -d @- ${MM_URL}/api/v4/posts
+message='{"id":"'"${MM_POST_ID}"'", "is_pinned":true, "message":"'Mensa\\n\\n"${food_mensa}"\\n\\nMri\\n\\n"${food_mri}"'"}'
+echo $message | curl -X PUT -H "Authorization: Bearer ${BOT_TOKEN}" -H 'Content-Type: application/json' -d @- ${MM_URL}/api/v4/posts/${MM_POST_ID}
