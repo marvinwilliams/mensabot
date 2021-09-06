8c7cc2a1aab93c366662bea4720dbd9a99041679
Marvin <marvinwilliams95@gmail.com>
2021-09-06 16:59:32 +0200

    fixed formatting and pinning

--- post_mensa_menu.sh
+++ post_mensa_menu.sh
@@ -14,5 +14,5 @@ last_message_id="$(cat /var/local/lib/mensabot_last_message_id)"
 
 curl -X PUT -H "Authorization: Bearer ${BOT_TOKEN}" -H 'Content-Type: application/json' -d '{"is_pinned":false}' ${MM_URL}/api/v4/posts/"${last_message_id}"/patch
 
-message='{"channel_id":"'"${MM_CHANNEL_ID}"'", "is_pinned":true, "message":"'Mentions: ${MM_MENTIONS}\\n\\nMRI\\n\\n"${food_mri}"\\n\\nMensa\\n\\n"${food_mensa}"'"}'
-echo $message | curl -X POST -H "Authorization: Bearer ${BOT_TOKEN}" -H 'Content-Type: application/json' -d @- ${MM_URL}/api/v4/posts | jq ".id" > /var/local/lib/mensabot_last_message_id
+message='{"channel_id":"'"${MM_CHANNEL_ID}"'", "is_pinned":true, "message":"'Mentions:\\n"${MM_MENTIONS}"\\nMRI\\n\\n"${food_mri}"\\n\\nMensa\\n\\n"${food_mensa}"'"}'
+echo $message | curl -X POST -H "Authorization: Bearer ${BOT_TOKEN}" -H 'Content-Type: application/json' -d @- ${MM_URL}/api/v4/posts | jq -r ".id" > /var/local/lib/mensabot_last_message_id
