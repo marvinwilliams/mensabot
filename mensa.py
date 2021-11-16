000f596369d2b22f152dcdc46237902ef3d55808
Niklas Uhl <niklas.uhl@online.de>
2021-11-16 23:01:40 +0100

    Fixed mensa parser (for now ...)

--- mensa.py
+++ mensa.py
@@ -5,32 +5,29 @@ import sys
 url = sys.argv[1]
 meals = {}
 r = requests.get(url)
+r.raise_for_status()
 soup = BeautifulSoup(r.text, 'html.parser')
 adenauer = soup.find(id='canteen_place_1')
-cantine = adenauer.find(id='fragment-c1-1')#.find_all('tr', recursive=False)
+cantine = adenauer.find(id='fragment-c1-1')
 line = ''
 meal_name = ''
-for row in cantine.find_all('tr', recursive=False):
-    for column in row.find_all('td'):
-        if not 'class' in column.attrs:
-            continue
-        if 'mensatype' in column['class']:
-            line = column.div.text
-            meals[line] = {}
-        for entry in column.find_all('span'):
-            if 'class' in entry.attrs:
-                if 'bg' in entry['class']:
-                    if not entry.text:
-                        continue
-                    meal_name = entry.text
-                    meals[line][meal_name] = ''
-                    print(line, meal_name)
-                elif 'price_3' in entry['class']:
-                    if not entry.text:
-                        continue
-                    price = entry.text 
-                    meals[line][meal_name] = price
-
+for entry in cantine.find_all(['span', 'td']):
+    if entry.name == 'td' and 'class' in entry.attrs and 'mensatype' in entry['class']:
+        line = entry.div.text
+    if not line in meals:
+        meals[line] = {}
+    if 'class' in entry.attrs:
+        if 'bg' in entry['class']:
+            if not entry.text:
+                continue
+            meal_name = entry.text
+            if not meal_name in meals[line]:
+                meals[line][meal_name] = ''
+        elif 'price_3' in entry['class']:
+            if not entry.text:
+                continue
+            price = entry.text 
+            meals[line][meal_name] = price
 
 print("| Linie | Essen | Preis |\\n")
 print("|-------|-------|-------|\\n")
