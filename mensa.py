b223b05882502044270ba582d1d70a45b2abff1c
Tim Niklas Uhl <tim.uhl@kit.edu>
2021-11-16 21:45:06 +0100

    WIP

--- mensa.py
+++ mensa.py
@@ -7,27 +7,29 @@ meals = {}
 r = requests.get(url)
 soup = BeautifulSoup(r.text, 'html.parser')
 adenauer = soup.find(id='canteen_place_1')
-cantine = adenauer.find(id='fragment-c1-1').table
+cantine = adenauer.find(id='fragment-c1-1')#.find_all('tr', recursive=False)
 line = ''
 meal_name = ''
-for row in cantine.contents:
-    for column in row.children:
+for row in cantine.find_all('tr', recursive=False):
+    for column in row.find_all('td'):
+        if not 'class' in column.attrs:
+            continue
         if 'mensatype' in column['class']:
             line = column.div.text
             meals[line] = {}
-        if 'mensadata' in column['class']:
-            for entry in column.table.find_all('span'):
-                if 'class' in entry.attrs:
-                    if 'bg' in entry['class']:
-                        if not entry.text:
-                            continue
-                        meal_name = entry.text
-                        meals[line][meal_name] = ''
-                    elif 'price_3' in entry['class']:
-                        if not entry.text:
-                            continue
-                        price = entry.text 
-                        meals[line][meal_name] = price
+        for entry in column.find_all('span'):
+            if 'class' in entry.attrs:
+                if 'bg' in entry['class']:
+                    if not entry.text:
+                        continue
+                    meal_name = entry.text
+                    meals[line][meal_name] = ''
+                    print(line, meal_name)
+                elif 'price_3' in entry['class']:
+                    if not entry.text:
+                        continue
+                    price = entry.text 
+                    meals[line][meal_name] = price
 
 
 print("| Linie | Essen | Preis |\\n")
